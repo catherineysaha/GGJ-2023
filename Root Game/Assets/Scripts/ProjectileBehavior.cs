ad7677f9aa72b7864bc68d6d1010b5aad5d01bcc
Justin Lewis <jlewisait@gmail.com>
2023-02-04 13:11:01 -0500

    Added projectile bouncing

--- Root Game/Assets/Scripts/ProjectileBehavior.cs
+++ Root Game/Assets/Scripts/ProjectileBehavior.cs
@@ -4,15 +4,27 @@ using UnityEngine;
 
 public class ProjectileBehavior : MonoBehaviour
 {
+    private Rigidbody2D rb;
+    private Vector3 lastVelocity;
+
     // Start is called before the first frame update
     void Start()
     {
+        rb = GetComponent<Rigidbody2D>();
         Destroy(gameObject, 5);
     }
 
     // Update is called once per frame
     void Update()
     {
-        
+        lastVelocity = rb.velocity;
+    }
+
+    private void OnCollisionEnter2D(Collision2D collision) {
+        Debug.Log("here");
+        float speed = lastVelocity.magnitude;
+        Vector3 direction = Vector3.Reflect(lastVelocity.normalized, collision.contacts[0].normal);
+
+        rb.velocity = direction * Mathf.Max(speed, 0f);
     }
 }
